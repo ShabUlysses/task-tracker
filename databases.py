4dde72d256c1631a21bcb00cfe0a8a2268070c55
ShabUlysses <shab.ulysses@gmail.com>
2022-05-01 22:08:43 +0300

    Created tables for datastorage

---
+++ databases.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+
+from app import app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pmapp.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    projects = db.relationship('Project', backref='manager', lazy=True)
+    tasks = db.relationship('Task', backref='executor', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
+
+class Project(db.Model):
+    __tablename__ = "project"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    users = db.Column(db.String(250), nullable=False)
+    task = db.relationship('Task', cascade='all, delete-orphan')
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_end = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Project name:('{self.name}', manager:'{self.manager}')"
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name
+        }
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    content = db.Column(db.String(500))
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_due = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Task name:('{self.name}', executor:'{self.manager}')"
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'content': self.content
+        }
+
+
