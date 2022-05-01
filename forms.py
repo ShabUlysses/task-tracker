2b51dc5b7bc1be58709a1a9f3f0cf8d4d0c541e7
ShabUlysses <shab.ulysses@gmail.com>
2022-05-01 22:08:26 +0300

    Changed direct links to url_for

--- forms.py
+++ forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+    name = StringField('Name',
+                           validators=[DataRequired(), Length(min=2, max=250)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password',
