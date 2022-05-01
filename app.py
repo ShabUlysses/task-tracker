f7350a2f5467da025d87125dd4e95b84347597ec
ShabUlysses <shab.ulysses@gmail.com>
2022-05-01 16:40:47 +0300

    Made changes to @app.route('/register')

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
+from config import SECRET_KEY
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route("/")
 def hello():
@@ -13,7 +16,21 @@ def index():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('publicindex.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)\
+
+@app.route('/login')
+def login():
+    form = RegistrationForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
