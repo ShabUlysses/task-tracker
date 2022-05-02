cb56cec0dce9c2179138b3b79e750947df38e75f
ShabUlysses <shab.ulysses@gmail.com>
2022-05-03 00:49:44 +0300

    Added validation by email for new users

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from main.models import User
 
 class RegistrationForm(FlaskForm):
     name = StringField('Name',
@@ -12,6 +13,11 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data.lower()).first()
+        if email:
+            raise ValidationError('That email is taken. Please choose different one.')
 
 
 class LoginForm(FlaskForm):
